e1e1f33c2a29606d83e663544454e86055fcbefd
Omar Mahat <omar.mahat@student.moringaschool.com>
2022-08-15 15:07:53 +0300

    commit tree e390c2368aa0b5f50805abb8f3a609a37f5313f8

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+def greet_programmer() 
+    puts "Hello, programmer!"
+end
+greet_programmer()
+
+def greet(name)
+    puts ("Hello, #{name}!");
+end
+greet("Omar")
+
+def greet_with_default(name = "programmer")
+    puts("Hello, #{name}!");
+end
+greet_with_default("Omar")
+
+def add(num1, num2)
+    num1 + num2;
+end
+add(2, 5)
+
+def halve(number)
+    if (number.class != Integer ) 
+        return nil;
+    end
+    return number / 2;
+end
+halve(4)
